b2b34289bcdd62deda92233c5f240b2d199d11bf
Michael Kessel <mjkessel@gmail.com>
2015-07-25 10:42:03 -0700

    Added Shared Prefenences

--- app/src/main/java/com/mjkessel/android/sunshine/ForecastFragment.java
+++ app/src/main/java/com/mjkessel/android/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.mjkessel.android.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -62,6 +64,12 @@ public class ForecastFragment extends android.support.v4.app.Fragment {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
